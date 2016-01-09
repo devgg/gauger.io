5b7b3e9fd2dbd339b41793f26fdeb31b450952d5
devgg <flo.gauger@gmail.com>
2016-01-09 23:42:27 +0100

    add database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,90 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const lowdb = require('lowdb')('model/db.json', { storage: require('lowdb/file-sync') })
 
 
-
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    var categoryMap = new Map();
-    categoryMap.set('C++', "cpp");
-    categoryMap.set('Java', "java");
-    categoryMap.set('JavaScript', "javascript");
+    var db = lowdb.read().object;
+    var categories = new Map();
+    for (var i = 0; i < db.category.length; i++) {
+        categories.set(db.category[i].simpleName, db.category[i].displayName);
+    }
     res.render('index', {
-        title: 'Express',
-        articles: [
-            {
-                head: 'Lorem ipsum dolor sit amet, velit ridens',
-                text: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare te mel, eu omittam omnesque posidonium quo. An dolor principes accommodare nam, in nec veniam vocent temporibus. Fabulas patrioque vituperata at sea, nisl sanctus appareat ei cum, vero option recusabo ei mea. No pri utinam delenit, his atqui aliquam no, no his mediocrem expetenda. Alienum temporibus in duo, malorum partiendo per cu.',
-                categories: [
-                    {
-                        name: 'C++',
-                    },
-                    {
-                        name: 'Java',
-                    },
-                    {
-                        name: 'JavaScript',
-                    }
-
-                ],
-                date: '9 January 2016'
-            },
-            {
-                head: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare',
-                text: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare te mel, eu omittam omnesque posidonium quo. An dolor principes accommodare nam, in nec veniam vocent temporibus. Fabulas patrioque vituperata at sea, nisl sanctus appareat ei cum, vero option recusabo ei mea. No pri utinam delenit, his atqui aliquam no, no his mediocrem expetenda. Alienum temporibus in duo, malorum partiendo per cu.',
-                categories: [
-                    {
-                        name: 'C++',
-                    },
-                    {
-                        name: 'JavaScript',
-                    }
-                ],
-                date: '9 January 2016'
-            },
-            {
-                head: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare',
-                text: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare te mel, eu omittam omnesque posidonium quo. An dolor principes accommodare nam, in nec veniam vocent temporibus. Fabulas patrioque vituperata at sea, nisl sanctus appareat ei cum, vero option recusabo ei mea. No pri utinam delenit, his atqui aliquam no, no his mediocrem expetenda. Alienum temporibus in duo, malorum partiendo per cu.',
-                categories: [
-                    {
-                        name: 'C++',
-                    },
-                    {
-                        name: 'JavaScript',
-                    }
-                ],
-                date: '9 January 2016'
-            },
-            {
-                head: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare',
-                text: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare te mel, eu omittam omnesque posidonium quo. An dolor principes accommodare nam, in nec veniam vocent temporibus. Fabulas patrioque vituperata at sea, nisl sanctus appareat ei cum, vero option recusabo ei mea. No pri utinam delenit, his atqui aliquam no, no his mediocrem expetenda. Alienum temporibus in duo, malorum partiendo per cu.',
-                categories: [
-                    {
-                        name: 'C++',
-                    },
-                    {
-                        name: 'JavaScript',
-                    }
-                ],
-                date: '9 January 2016'
-            },
-            {
-                head: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare',
-                text: 'Lorem ipsum dolor sit amet, velit ridens labore has ea. Tibique repudiare te mel, eu omittam omnesque posidonium quo. An dolor principes accommodare nam, in nec veniam vocent temporibus. Fabulas patrioque vituperata at sea, nisl sanctus appareat ei cum, vero option recusabo ei mea. No pri utinam delenit, his atqui aliquam no, no his mediocrem expetenda. Alienum temporibus in duo, malorum partiendo per cu.',
-                categories: [
-                    {
-                        name: 'C++',
-                    },
-                    {
-                        name: 'JavaScript',
-                    }
-                ],
-                date: '9 January 2016'
-            }
-        ],
-        categoryMap: categoryMap
+        title: 'Florian Gauger',
+        articles: db.article,
+        categories: categories
     });
 });
 
