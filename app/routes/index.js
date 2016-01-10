2d8bd520db1cd2a0875203c51b145ffa44bec126
devgg <flo.gauger@gmail.com>
2016-01-10 13:08:39 +0100

    add blog navigation

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,32 +4,37 @@ var router = express.Router();
 const lowdb = require('lowdb')('model/db.json', { storage: require('lowdb/file-sync') })
 
 
-
-router.get(['/', '/:page(\\b\\B|\\d*)'] , function(req, res, next) {
-    var articlesPerPage = 10;
+function calculateSiteConfiguration(page, articlesPerPage, filter) {
     var db = lowdb.read().object;
-    var page = req.params.page === undefined? 1 : parseInt(req.params.page);
+    page = page === undefined? 1 : parseInt(page);
+    var pageMax = Math.floor(db.article.length / articlesPerPage) + 1;
     if (page == 0) {
         page = 1;
     } else if (page > db.article.length / articlesPerPage) {
-        page = Math.floor(db.article.length / articlesPerPage) + 1;
+        page = pageMax;
     }
+    var articleIndexFirst = (page - 1) * articlesPerPage;
+    var articleIndexLast = db.article.length < page * articlesPerPage ? db.article.length : page * articlesPerPage;
+    var articles = db.article.slice(articleIndexFirst, articleIndexLast);
 
     var categories = new Map();
     for (var i = 0; i < db.category.length; i++) {
         categories.set(db.category[i].simpleName, db.category[i].displayName);
     }
-    var articleIndexFirst = (page - 1) * articlesPerPage;
-    var articleIndexLast = db.article.length < page * articlesPerPage ? db.article.length : page * articlesPerPage;
-    var articles = db.article.slice(articleIndexFirst, articleIndexLast);
-    res.render('index', {
-        title: 'Florian Gauger',
+    return {
         articles: articles,
         categories: categories,
         page: page,
+        pageMax: pageMax,
         showButtonNext: articleIndexLast < db.article.length,
         showButtonPrevious: articleIndexFirst != 0
-    });
+    }
+}
+
+router.get(['/', '/:page(\\b\\B|\\d*)'] , function(req, res, next) {
+    var siteConfiguration = calculateSiteConfiguration(req.params.page, 10);
+    siteConfiguration.title = 'Florian Gauger';
+    res.render('index', siteConfiguration);
 });
 
 module.exports = router;
