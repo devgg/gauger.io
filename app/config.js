52d6df25d9984f92350282b10de7fb8b37f45cbb
devgg <flo.gauger@gmail.com>
2016-01-18 22:15:09 +0100

    change directory setup

--- app/config.js
+++ app/config.js
@@ -12,7 +12,7 @@ config.paths.views = path.join(workingDirectory, 'views');
 config.paths.public = path.join(workingDirectory, 'public');
 config.paths.subpages = path.join(workingDirectory, 'subpages');
 
-config.paths.images = path.join(config.paths.public, 'images');
+config.paths.images = path.join(config.paths.public, 'img');
 config.paths.favicon = path.join(config.paths.images, 'favicon.ico');
 config.paths.app = path.join(workingDirectory, 'app');
 
