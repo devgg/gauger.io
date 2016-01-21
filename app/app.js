5bce79ffcdcd04ba5cad34c29f332d4e959fd9a2
Florian Gauger <Flo.Gauger@gmail.com>
2016-01-21 22:57:00 +0100

    change route setup

--- app/app.js
+++ app/app.js
@@ -1,31 +1,30 @@
 var express = require('express');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var favicon = require('serve-favicon');
 var stylus = require('stylus');
 var axis = require('axis');
 
 var config = require('./config.js');
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+var blog = require('./routes/blog');
+var bibsbn = require('./routes/bibsbn');
 
 
 const lowdb = require('lowdb');
 const gradientsDb = lowdb(config.paths.bibsbn + '/model/gradients.json', { storage: require('lowdb/file-sync') })
 const gradients = gradientsDb.read().object;
-var gradientIndex;
 
 var app = express();
 
-
-app.locals.foo = config.paths.images;
-
-// view engine setup
 app.set('views', config.paths.views);
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+
+app.use('/blog', blog);
+app.use('/bibsbn', bibsbn);
+
+
 //app.use(favicon(config.paths.favicon));
-app.use(logger('dev'));
+
 
 
 
@@ -46,7 +45,6 @@ app.use(express.static(config.paths.public));
 app.use(express.static(config.paths.subpages + '/bibsbn/public'));
 
 function compile(str, path){
-    console.log(path);
     var gradient = gradients[Math.floor(Math.random() * gradients.length)];
     return stylus(str)
         .set('filename', path)
@@ -56,10 +54,6 @@ function compile(str, path){
 }
 
 
-//app.use(express.static(config.paths.public + '/subpages'));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -68,7 +62,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
 // development error handler
 // will print stacktrace
---
+++ app/routes/blog.js
@@ -0,0 +1,39 @@
+
+var router = require('express').Router();
+var config = require('../config.js');
+const blogDb = require('lowdb')(config.paths.model + '/db.json', { storage: require('lowdb/file-sync') })
+
+function calculateSiteConfiguration(page, articlesPerPage, filter) {
+    var db = blogDb.read().object;
+    page = page === undefined? 1 : parseInt(page);
+    var pageMax = Math.floor(db.article.length / articlesPerPage) + 1;
+    if (page == 0) {
+        page = 1;
+    } else if (page > db.article.length / articlesPerPage) {
+        page = pageMax;
+    }
+    var articleIndexFirst = (page - 1) * articlesPerPage;
+    var articleIndexLast = db.article.length < page * articlesPerPage ? db.article.length : page * articlesPerPage;
+    var articles = db.article.slice(articleIndexFirst, articleIndexLast);
+
+    var categories = new Map();
+    for (var i = 0; i < db.category.length; i++) {
+        categories.set(db.category[i].simpleName, db.category[i].displayName);
+    }
+    return {
+        articles: articles,
+        categories: categories,
+        page: page,
+        pageMax: pageMax,
+        showButtonNext: articleIndexLast < db.article.length,
+        showButtonPrevious: articleIndexFirst != 0
+    }
+}
+
+router.get(['/', '/:page(\\b\\B|\\d*)'] , function(req, res, next) {
+    var siteConfiguration = calculateSiteConfiguration(req.params.page, 10);
+    siteConfiguration.title = 'Florian Gauger';
+    res.render('index', siteConfiguration);
+});
+
+module.exports = router;
