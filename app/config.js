ef1f57a007acd5eba886bcdd0c9b9cf1555c76e4
Florian Gauger <Flo.Gauger@gmail.com>
2016-01-23 15:09:50 +0100

    move blog database

--- app/config.js
+++ app/config.js
@@ -10,7 +10,6 @@ var workingDirectory = path.join(__dirname, '..');
 
 config.paths.views = path.join(workingDirectory, 'views');
 config.paths.public = path.join(workingDirectory, 'public');
-config.paths.model = path.join(workingDirectory, 'model');
 config.paths.subpages = path.join(workingDirectory, 'subpages');
 config.paths.bibsbn = path.join(config.paths.subpages, 'bibsbn');
 config.paths.blog = path.join(config.paths.subpages, 'blog');
--- app/routes/blog.js
+++ app/routes/blog.js
@@ -1,31 +1,30 @@
 
 var router = require('express').Router();
 var config = require('../config.js');
-const blogDb = require('lowdb')(config.paths.model + '/db.json', { storage: require('lowdb/file-sync') })
+const posts = require('lowdb')(config.paths.blog + '/model/db.json', { storage: require('lowdb/file-sync') }).read().object;
 
 function calculateSiteConfiguration(page, articlesPerPage, filter) {
-    var db = blogDb.read().object;
     page = page === undefined? 1 : parseInt(page);
-    var pageMax = Math.floor(db.article.length / articlesPerPage) + 1;
+    var pageMax = Math.floor(posts.article.length / articlesPerPage) + 1;
     if (page == 0) {
         page = 1;
-    } else if (page > db.article.length / articlesPerPage) {
+    } else if (page > posts.article.length / articlesPerPage) {
         page = pageMax;
     }
     var articleIndexFirst = (page - 1) * articlesPerPage;
-    var articleIndexLast = db.article.length < page * articlesPerPage ? db.article.length : page * articlesPerPage;
-    var articles = db.article.slice(articleIndexFirst, articleIndexLast);
+    var articleIndexLast = posts.article.length < page * articlesPerPage ? posts.article.length : page * articlesPerPage;
+    var articles = posts.article.slice(articleIndexFirst, articleIndexLast);
 
     var categories = new Map();
-    for (var i = 0; i < db.category.length; i++) {
-        categories.set(db.category[i].simpleName, db.category[i].displayName);
+    for (var i = 0; i < posts.category.length; i++) {
+        categories.set(posts.category[i].simpleName, posts.category[i].displayName);
     }
     return {
         articles: articles,
         categories: categories,
         page: page,
         pageMax: pageMax,
-        showButtonNext: articleIndexLast < db.article.length,
+        showButtonNext: articleIndexLast < posts.article.length,
         showButtonPrevious: articleIndexFirst != 0
     }
 }
